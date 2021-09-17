d18490d5e633678b86c299543c19d33027003889
jfgonzalezj <jf.gonzalezj@uniandes.edu.co>
2021-09-17 13:26:24 -0500

    Update Ejercicio 4.py

--- Proyecto especial/Conservación de energía/Ejercicio 4.py
+++ Proyecto especial/Conservación de energía/Ejercicio 4.py
@@ -9,7 +9,7 @@ import numpy as np
 import sympy as sp
 from matplotlib import pyplot as plt
 from sympy import *
-b, y1, v1, z1, y2, v2, z2, Q, yin, m, yc = symbols('b y1 v1 z1 y2 v2 z2 Q yin m yc')
+b, y1, v1, z1, y2, v2, z2, Q, yin, m, yc, Ec = symbols('b y1 v1 z1 y2 v2 z2 Q yin m yc Ec')
 
 'Contracción del canal'
 '-----------------------------------------------------------------------------'
@@ -23,7 +23,7 @@ b2 = float(input("Base final del canal (b2): "))
 v1 = float(input("Velocidad inicial del agua (v1): "))
 '''
 'Variables por default necesarias para las funciones'
-inc= 1
+
 m1 = 1
 m2 = 1
 
@@ -37,7 +37,7 @@ b2 = 10
 v1 = 1.05
 
 '-----------------------------------------------------------------------------'
-'Datos adicionales dependiente del tipo de figura'
+'Datos adicionales dependiente del tipo de sección'
 
 if Figura == "Trapecial":
     
@@ -53,14 +53,13 @@ if Figura == "Trapecial":
         m1 = float(input("Inclinación (m) lado izquierdo: "))
         m2 = float(input("Inclinación (m) lado derecho: "))
 
-def Area(y,b,inc,m1,m2):
+def Area(y,b,m1,m2):
     
     """ Esta función retorna el área transversal según la figura\n
         
     Parámetros:
         y (float) altura del agua
         b (float) base del canal
-        inc (float) grados o inclinación de la sección triangular
         m1 (float) grados o inclinación parte izquierda de un trapecio
         m2 (float) grados o inclinación parte derecha de un trapecio
     Retorna:
@@ -70,16 +69,6 @@ def Area(y,b,inc,m1,m2):
     if Figura == "Rectangular":
         
         A = b * y
-        
-    if Figura == "Triangular":
-        
-        if incT == "alpha" or incT == "Alpha":
-            
-            A = y**2/np.tan(inc*np.pi/180)
-            
-        else:
-            
-            A = y**2 * inc
             
     if Figura == "Trapecial":
         
@@ -105,35 +94,78 @@ def Area(y,b,inc,m1,m2):
             
     return A
 
-def Qfun(y,b,inc,m1,m2):
+
+
+def Tfun(y,b,m1,m2):
     
     """ Esta función retorna el caudal según la sección transversal\n
         
     Parámetros:
         y (float) altura del agua
         b (float) base del canal
-        inc (float) grados o inclinación de la sección triangular
+        m1 (float) grados o inclinación parte izquierda de un trapecio
+        m2 (float) grados o inclinación parte derecha de un trapecio
+    Retorna:
+        float: El cuadal de la sección transversal [m^3/s]
+    """
+
+    if Figura == "Rectangular":
+        
+        T = b
+
+    if Figura == "Trapecial":
+        
+        
+        if incTraT == "alpha" or incTraT == "Alpha":
+            
+            if m1 == m2:
+                
+                T = b + 2 * (y/np.tan(np.pi/180*m1))
+                
+            else:
+                
+                T = b + (y/np.tan(np.pi/180*m1)) + (y/np.tan(np.pi/180*m2))
+        else:
+            
+            if m1 == m2:
+                
+                T = b + 2 * (m1*y)
+                
+            else:
+                
+                T = b + (m1*y) + (m2*y)
+
+    return T
+    
+
+def Qfun(y,b,m1,m2):
+    
+    """ Esta función retorna el caudal según la sección transversal\n
+        
+    Parámetros:
+        y (float) altura del agua
+        b (float) base del canal
         m1 (float) grados o inclinación parte izquierda de un trapecio
         m2 (float) grados o inclinación parte derecha de un trapecio
     Retorna:
         float: El cuadal de la sección transversal [m^3/s]
     """
     
-    Q = v1 * Area(y,b,inc,m1,m2) 
+    Q = v1 * Area(y,b,m1,m2) 
     
     return Q
+
 
 def y2fun(y2):
     
     """ Esta función retorna la altura del agua en la sección dos\n
         
     Parámetros:
-        y1 (symbol) variable que se quiere calcular
         y2 (symbol) variable que se quiere calcular
     Retorna:
         float: La altura del agua en la sección 2 [m]
     """
-    Q = Qfun(y1,b1,inc,m1,m2)
+    Q = Qfun(y1,b1,m1,m2)
     
     if Figura == "Rectangular":
         
@@ -180,92 +212,59 @@ def y2fun(y2):
     
         else:
             
-            y.append(round(y2[i],3))
+            temp = "Fenomeno de choque, se requiere calcular yc\n"
+                     
+            return temp
+            
         i+=1
         
     return y 
 
-print(y2fun(y2))
 
-def calculo_v2():
-    
-    i=0
-    
-    while i<len(y2fun(y2))-1:
-        
-        if im(y2fun(y2)[i]) == 0 :
-            ecu = Eq(Qfun(y1,b1,inc,m1,m2),v2*Area(y2fun(y2)[2],b2,inc,m1,m2))
-            v = solve(ecu)
-        else:
-            return "Error, fenomeno de choque"
-        i+=1
-    return v
-
-print(calculo_v2())
 
 def calculo_yc(yc):
     
-    
-    if calculo_v2() == "Error, fenomeno de choque":
-        return "Error, fenomeno de choque"
-    
-    v2 = calculo_v2()[0]
-    y2 = y2fun(yc)[len(y2fun(yc))-1]
-    
-    if Figura == "Rectangular":
-        
-        ec1 = Eq(yc,((v2**2*(b2*y2)**2)/(b2**2*g))**(1/3))
-    
-        yc = solve(ec1)  
-    
-    if Figura == "Trapecial":
-        
-        if incTraT == "alpha" or incTraT == "Alpha":
+    Q = Qfun(y1,b1,m1,m2)
+    ec1 = Eq(Q/sqrt(g),(Area(yc, b2, m1, m2)*sqrt(Area(yc, b2, m1, m2)))/sqrt(Tfun(yc, b2, m1, m2)))
             
-            if m1 == m2:
-                
-                ec1 = Eq(yc, ((v2**2*(y2*b2+y2**2/np.tan(np.pi/180*m1))**2)/(b2**2*g))**(1/3))
+    yc = solve(ec1)[0]
     
-                yc = solve(ec1)
-                
-            else:
-                
-                ec1 = Eq(yc,((v2**2*(y2**2/(2*np.tan(np.pi/180*m1)) + b2*y2 + y2**2/(2*np.tan(np.pi/180*m2)))**2)/(b2**2*g))**(1/3))
-    
-                yc = solve(ec1) 
-        else:
-            
-            if m1 == m2:
-                
-                ec1 = Eq(yc,((v2*((b2+m1*y2)*y2)**2)/(b2**2*g))**(1/3))
-    
-                yc = solve(ec1)
-                
-            else:
-                ec1 = Eq(yc,((v2*(m1*y2**2/2+b2*y2+m2*y2**2/2)**2)/(b2**2*g))**(1/3))
-            
-                yc = solve(ec1)
     return yc
 
-print(calculo_yc(yc))
 
-'¿Cómo calculo yc para una sección que no sea rectangular?'
-'v depende de y2 pero y2 puede ser imaginario'
-def calculo_Ec(yc):
+
+def calculo_v2():
     
-    try:
-        Ec = (3/2)*calculo_yc(yc)[0]
-    except:
-        return print("Error")
-        
+    
+    if y2fun(y2) == "Fenomeno de choque, se requiere calcular yc\n":
+        ecu = Eq(Qfun(y1,b1,m1,m2),v2*Area(calculo_yc(yc),b2,m1,m2))
+        v = solve(ecu)
+    else:
+        if Figura == "Rectangular":
+            
+            ecu = Eq(Qfun(y1,b1,m1,m2),v2*Area(y2fun(y2)[2],b2,m1,m2))
+            v = solve(ecu)
+        else:
+            ecu = Eq(Qfun(y1,b1,m1,m2),v2*Area(max(y2fun(y2)),b2,m1,m2))
+            v = solve(ecu)
+    
+    return v
+
+
+def calculo_Ec(Ec, yc):
+    
+    ecu = Eq(Ec,calculo_yc(yc) + calculo_v2()[0]**2/(2*g))
+    
+    Ec = solve(ecu)
+    
     return Ec
 
-print(calculo_Ec(yc))
 
-def calculo_yin(yin):
+'Error'
+def calculo_yin(Ec,yin):
     
-    Q = Qfun(y1,b1,inc,m1,m2)
-    Ec = calculo_Ec(yin)
+    Q = Qfun(y1,b1,m1,m2)
+    Ec = calculo_Ec(Ec,yin)[0]
     
     if Figura == "Rectangular":
         
@@ -301,46 +300,27 @@ def calculo_yin(yin):
             
                 yin = solve(ec1)
     return yin
-    
-print(calculo_yin(yin))
 
+def grafica4 (m):
 
-ec2 = Eq(Ec,yin+((q1**2)/(2*g*yin**2)))
-
-yin = solve(ec2)
-
-#print(round(yin[0],4))
-#print(round(yin[1],4))
-#print(round(yin[2],4))
-
-print(Ec, yc)
-
-EqCri = Eq(m,(0-yc)/(0-Ec))
-
-m = solve(EqCri)
-
-print(m)
-
-
-
-yin = yin[2]
-
-def grafica4 (qg1,qg2):
+    Q = Qfun(y1,b1,m1,m2)
+    EqCri = Eq(m,(0-calculo_yc(yc))/(0-calculo_Ec(Ec,yc)[0]))
+    m = solve(EqCri)
 
     x = np.linspace(0,10,50)
 
     yg = np.linspace(0.1,10,1000)
     yg2 = np.linspace(0.4,10,1000)
     
-    E = yg + (qg1)**2/(2*g*yg**2) 
-    E2 = yg2 + (qg2)**2/(2*g*yg2**2)
+    E = yg + (Q**2/(2*g*(Area(yg,b1,m1,m2))**2))
+    E2 = yg + (Q**2/(2*g*(Area(yg,b2,m1,m2))**2))
     
 
     plt.style.use('ggplot')
     plt.plot(E,yg,label = 'S1')
     plt.plot(E2,yg,label = 'S2')
-    plt.plot(x,m*x,label = 'Ec = 3/2 yc')
-    plt.plot(x,x)
+    plt.plot(x,x, label = 'E = y',linestyle='dashed')
+    plt.plot(x,m*x,label = 'Ec = 3/2 yc', linestyle='dashed')    
     plt.xlabel('E (m)')
     plt.ylabel('y (m)')
     plt.xlim(0,10)
@@ -348,9 +328,26 @@ def grafica4 (qg1,qg2):
     plt.legend()
     plt.show()
 
-grafica4(q1,q2)
 
+def imprimir_valores():
+    
+    print('Area1 ', Area(y1,b1,m1,m2))
+    
+    print('Caudal ', Qfun(y1,b1,m1,m2))
+    
+    print('valores de y2 ',y2fun(y2))
+    
+    print('valor de yc', calculo_yc(yc))
+    
+    print('valor de Ec',calculo_Ec(Ec, yc))
 
+    print('velocidad en 2 ',calculo_v2())   
+    
+    print(calculo_yin(Ec,yin))
+   
+    grafica4(m)
+
+print(imprimir_valores())
 
 
 
